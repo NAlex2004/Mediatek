ec1469bbf1724a6b0a1773b773b0bd7527f84ad6
alien <NAlex2004@gmail.com>
2017-04-08 17:27:58 +0300

    #3

--- LibMediatek/Interfaces/IMediaRepository.cs
+++ LibMediatek/Interfaces/IMediaRepository.cs
@@ -2,14 +2,14 @@
 
 namespace LibMediatek.Interfaces
 {
-    public interface IMediaCollection : IEnumerable<IMediaItem>
+    public interface IMediaRepository : IEnumerable<IMediaItem>
     {
         IMediaItem this[int index] { get; }
         IMediaItem this[string title] { get; }
         void Clear();
         void Add(IMediaItem item);
         bool Remove(IMediaItem item);
-        // some kind of update
+        /// some kind of update
         bool Replace(IMediaItem oldItem, IMediaItem newItem);
     }
 }
