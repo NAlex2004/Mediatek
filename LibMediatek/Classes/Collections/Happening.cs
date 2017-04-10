35a16f41d56cd7e36a5335f336a4a489d86665a0
NAlex2004 <nalex2004@gmail.com>
2017-04-10 16:43:52 +0300

    ..

---
+++ LibMediatek/Classes/Collections/Happening.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibMediatek.Interfaces;
+using LibMediatek.Classes.Items;
+using LibMediatek.Classes;
+using LibMediatek.Classes.Collections;
+
+namespace LibMediatek.Classes.Collections
+{
+    class Happening: Series
+    {
+
+    }
+}
