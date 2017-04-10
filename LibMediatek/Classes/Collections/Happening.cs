48b45f289c3c50c92a3551aee74a87bdf095b628
alien <NAlex2004@gmail.com>
2017-04-10 19:24:08 +0300

    Happening

--- LibMediatek/Classes/Collections/Happening.cs
+++ LibMediatek/Classes/Collections/Happening.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using LibMediatek.Interfaces;
+﻿using LibMediatek.Interfaces;
 using LibMediatek.Classes.Items;
-using LibMediatek.Classes;
-using LibMediatek.Classes.Collections;
+using LibMediatek.Classes.Factories;
 
 namespace LibMediatek.Classes.Collections
 {
     class Happening: Series
     {
+        public IWritableMediaCollection<VideoReference> VideoReferences { get; protected set; }
+        public IWritableMediaCollection<PhotoReference> ImageReferences { get; protected set; }
 
+        public Happening(MediaFactory factory) : base(factory)
+        {
+            VideoReferences = factory.CreateVideoReferences() as IWritableMediaCollection<VideoReference>;
+            ImageReferences = factory.CreateImageReferences() as IWritableMediaCollection<PhotoReference>;
+        }
     }
 }
